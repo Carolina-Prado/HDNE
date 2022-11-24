27d27a73f5e86a4a4f1ffa8b68bd1013afb63b6e
Maria de los Angeles Mejia Delgado <71246522+YukishaMA@users.noreply.github.com>
2022-11-24 17:36:30 -0600

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,131 @@
+<!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i> <!-- icono carita sonriendo -->
+                </div>
+                <div class="sidebar-brand-text mx-3">Equidad de Genero</sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+			<!-- Heading -->
+            <div class="sidebar-heading">
+                Anios
+            </div>
+
+            <li class="nav-item">
+				<a class="nav-link" href="dasboard2006.php">
+					<i class="fas fa-fw fa-chart-area"></i>
+					<span>2006</span></a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="dasboard2007.php">
+						<i class="fas fa-fw fa-chart-area"></i>
+						<span>2007</span></a>
+			</li>
+
+			 <li class="nav-item">
+				<a class="nav-link" href="dasboard2008.php">
+					<i class="fas fa-fw fa-chart-area"></i>
+					<span>2008</span></a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="dasboard2009.php">
+						<i class="fas fa-fw fa-chart-area"></i>
+						<span>2009</span></a>
+			</li>
+
+			 <li class="nav-item">
+				<a class="nav-link" href="dasboard2010.php">
+					<i class="fas fa-fw fa-chart-area"></i>
+					<span>2010</span></a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="dasboard2011.php">
+						<i class="fas fa-fw fa-chart-area"></i>
+						<span>2011</span></a>
+			</li>
+
+			 <li class="nav-item">
+				<a class="nav-link" href="dasboard2012.php">
+					<i class="fas fa-fw fa-chart-area"></i>
+					<span>2012</span></a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="dasboard2013.php">
+						<i class="fas fa-fw fa-chart-area"></i>
+						<span>2013</span></a>
+			</li>
+
+			 <li class="nav-item">
+				<a class="nav-link" href="dasboard2014.php">
+					<i class="fas fa-fw fa-chart-area"></i>
+					<span>2014</span></a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="dasboard2015.php">
+						<i class="fas fa-fw fa-chart-area"></i>
+						<span>2015</span></a>
+			</li>
+
+			 <li class="nav-item">
+				<a class="nav-link" href="dasboard2016.php">
+					<i class="fas fa-fw fa-chart-area"></i>
+					<span>2016</span></a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="dasboard2017.php">
+						<i class="fas fa-fw fa-chart-area"></i>
+						<span>2017</span></a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="dasboard2018.php">
+					<i class="fas fa-fw fa-chart-area"></i>
+					<span>2018</span></a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="dasboard2019.php">
+						<i class="fas fa-fw fa-chart-area"></i>
+						<span>2019</span></a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="dasboard2020.php">
+					<i class="fas fa-fw fa-chart-area"></i>
+					<span>2020</span></a>
+			</li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+            
+
+        </ul>
+<!-- End of Sidebar -->
